627621c6cf0843beb947ead870cc4f27a85e5f98
Otávio Araújo <otavio-luiz17@outlook.com>
2019-02-13 00:02:17 -0200

    Projeto salario medio

---
+++ Salario medio/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace Salario_medio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario f1 = new Funcionario();
+            Funcionario f2 = new Funcionario();
+
+            Console.WriteLine("Dados do primeiro funcionário: ");
+            Console.Write("Nome: ");
+            f1.Nome = Console.ReadLine();
+            Console.Write("Salário: ");
+            f1.Salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Dados do segundo funcionário:");
+            Console.Write("Nome: ");
+            f2.Nome = Console.ReadLine();
+            Console.Write("Salário: ");
+            f2.Salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double x;
+            x = (f1.Salario + f2.Salario) / 2.0;
+            Console.WriteLine("Salário Medio = " + x.ToString("F2", CultureInfo.InvariantCulture));
+
+            Console.ReadLine();
+        }
+    }
+}
